371bdf80af6252fe9d7d2548eeb5b2213fa2cc80
BathroomWithFen <77697491+BathroomWithFen@users.noreply.github.com>
2023-05-23 10:59:43 +0300

    Add files via upload

---
+++ tomcat10-jakartaee9-main/src/main/java/com/microsoft/example/HelloServlet.java
@@ -0,0 +1,24 @@
+package com.microsoft.example;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/helloservlet")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        
+        var out = resp.getOutputStream();
+        out.println("Hello!");
+        out.println();
+
+    }
+
+}
---
+++ tomcat10-jakartaee9-main/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+
+<body>
+    <h2>Hello this app is working</h2>
+    <a href="./helloservlet">Hit the link </a>
+</body>
+
+</html>
